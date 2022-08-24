51cf48cab956de10c24d6ba722c109e2720bbd9f
Deog <dmushi@sovereign-pay.com>
2022-08-24 07:38:52 +0300

    overview of django

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def calculate():
+    x = 1
+    y = 2 
+    return x
 
 # Create your views here.
+
+# view function it takes a request and return a response 
+#request handler
+
+def say_hello(request):
+    x = calculate()
+    return render(request, 'hello.html', {'name': 'Deo'})
